775ba0b8b68ca65e7dd8956082807c589032e449
kje123 <keith-3@hotmail.com>
2020-03-01 20:30:59 -0800

    added third chart

--- scripts/third_chart.R
+++ scripts/third_chart.R
@@ -1,10 +1,6 @@
 # Load the necessary packages
 library("lintr")
-library("tidyr")
 library("dplyr")
-library("knitr")
-library("ggplot2")
-library("leaflet")
 library("plotly")
 
 ## ASSIGNMENT INSTRUCTIONS ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -13,6 +9,35 @@ library("plotly")
 #   - Intentional chart type and encoding selection based on the question of
 #     interest and data type
 
-chart_3 <- function() {
-  
+g <- list(
+  scope = "usa",
+  projection = list(type = "albers usa"),
+  showland = TRUE,
+  landcolor = toRGB("gray95"),
+  subunitcolor = toRGB("gray85"),
+  countrycolor = toRGB("gray85"),
+  countrywidth = 0.5,
+  subunitwidth = 0.5
+)
+
+geo_chart <- function(df) {
+  grouped <- df %>%
+    group_by(State) %>%
+    summarise(
+      total_co = (sum(CO.Mean) * 1000),
+      total_no2 = sum(NO2.Mean),
+      total_o3 = (sum(O3.Mean) * 1000),
+      total_so2 = (sum(SO2.Mean)),
+      total_emm = sum(total_so2 + total_no2 + total_o3 + total_co)
+    ) %>%
+    mutate(state_abb = state.abb[match(State, state.name)])
+
+  fig <- plot_geo(grouped, locationmode = "USA-states") %>%
+    add_trace(
+      z = ~total_emm, locations = ~state_abb, color = ~total_emm
+    ) %>%
+    layout(geo = g, title = "US Total Pollution 2000 - 2016") %>%
+    colorbar(title = "Total Emissions (PPB)")
+
+  return(fig)
 }
