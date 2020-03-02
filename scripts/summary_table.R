40c58112e2640dfa20c8e1b62677327b2e94afd1
kje123 <keith-3@hotmail.com>
2020-03-01 18:42:15 -0800

    added summary table

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -1,11 +1,6 @@
 # Load the necessary packages
 library("lintr")
-library("tidyr")
 library("dplyr")
-library("knitr")
-library("ggplot2")
-library("leaflet")
-library("plotly")
 
 ## ASSIGNMENT INSTRUCTIONS ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Write a function that takes in a dataset as a parameter, and returns a table
@@ -16,7 +11,18 @@ library("plotly")
 #   - Only contain relevant information
 #   - Be intentionally sorted in a meaningful way
 
-
-get_summary_table <- function(dataset) {
-  
+get_summary_table <- function(df) {
+  grouped <- df %>%
+    group_by(State) %>%
+    summarise(
+      total_co = (sum(CO.Mean) * 1000),
+      total_no2 = sum(NO2.Mean),
+      total_o3 = (sum(O3.Mean) * 1000),
+      total_so2 = (sum(SO2.Mean)),
+      total_emm = sum(total_so2 + total_no2 + total_o3 + total_co)
+      ) %>%
+    arrange(desc(total_emm))
 }
+
+names_col <- c("State", "Total CO", "Total NO2",
+               "Total O3", "Total SO2", "Total Emmissions")
