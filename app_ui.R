59e744a7204cd9b92af9205ae37cdeef9e701b46
Audrey Kho <audreykho13@yahoo.com>
2020-03-11 01:01:30 -0700

    ui and css changes

--- app_ui.R
+++ app_ui.R
@@ -11,20 +11,25 @@ overview <- tabPanel(
   img(src = "image.jpg", class = "center"),
 
   h2("Questions we want to answer:"),
-  p("1. What states are contributing the most to U.S. emissions?"),
-  p("2. How has the progression of emissions been from 2000 to 2016?"),
-  p("3. What pollutants are the most prevalent?"),
+  tag$p(id = "skip_indent",
+    "1. What states are contributing the most to U.S. emissions?", br(),
+    "2. How has the progression of emissions been from 2000 to 2016?", br(),
+    "3. What pollutants are the most prevalent?"
+  ),
 
-  p("To answer these important questions, our group has created 3 data
+  p(
+    "To answer these important questions, our group has created 3 data
      visualizations in the second, third, and fourth tabs of this web
      application. Each of the visualizations display values pulled from a
      dataset compiled by the", strong(" U.S. Environmental Protection Agency
      (U.S. EPA)."), "The dataset contains information about", strong("emission
      levels of four different chemical compounds"), "that have contributed to
      the", strong("air pollution in each U.S. state in the years 2000-2016,"),
-     "namely: ", em("Carbon Monoxide (CO),"), em("Sulfur Dioxide (SO2),"),
-     em("Ozone (O3),"), "and", em("Nitrogen Dioxide (NO2)"), "."),
-  p("The", strong("\"Mapping the Emissions\" tab"), "contains our first
+    "namely: ", em("Carbon Monoxide (CO),"), em("Sulfur Dioxide (SO2),"),
+    em("Ozone (O3),"), "and", em("Nitrogen Dioxide (NO2)"), "."
+  ),
+  p(
+    "The", strong("\"Mapping the Emissions\" tab"), "contains our first
     visualization: a choropleth map, which is a color-coded map indicating
     the amount of the four air pollutant (individually and in total) that
     each U.S. state is responsible for. Next, the", strong("\"Emissions over
@@ -34,10 +39,12 @@ overview <- tabPanel(
     strong("\"Distribution of Emissions\" tab"), "contains a pie chart that
     presenting the average daily levels of each of the four compounds in
     relation to each other that were recorded in the atmosphere across the
-    United States from 2000-2016."),
+    United States from 2000-2016."
+  ),
 
   h2("Background information"),
-  p("Before going onto the next few tabs, we would like to provide a bit of
+  p(
+    "Before going onto the next few tabs, we would like to provide a bit of
     a background on the nature of the 4 air pollutants our data focuses on.
     The first gas,", strong("Carbon monoxide (CO)"), "is probably the most
     well-known out of the four as the cause of the most fatal type of air
@@ -63,7 +70,8 @@ overview <- tabPanel(
     but is also produced by the burning of fossil fuels and a few natural
     processes. Similar to the other gases, NO2 is also known to cause
     respiratory problems, most notably asthma, and also plays a role in acid
-    rain."),
+    rain."
+  ),
   p("To learn more about the atmospheric levels of these four air pollutants in
     the U.S. and their trends across time, please visit the next few tabs of
     this web application to view the visualizations of the data.")
@@ -73,7 +81,8 @@ page_one <- tabPanel(
   "Mapping the Emissions",
   titlePanel("A Map of the Different Types of Emissions in the U.S."),
 
-  p("This chart is a mapping of all the total emissions from 2000 to 2016.
+  p(
+    "This chart is a mapping of all the total emissions from 2000 to 2016.
     We included a widget that allows you to choose each individual different
     pollutant to see the impact of each one. The data is mapped onto a
     chloropleth map to show each state's differences comparatively. This
@@ -85,16 +94,19 @@ page_one <- tabPanel(
     Pennsylvania's long history of coal mining and burning for energy. What's
     interesting is that there are a few states that don't show up on the map,",
     strong("Montana, Mississippi, Nebraska and West Virginia."), "Looking at
-    the table we made earlier, there is no entries for these states either."),
+    the table we made earlier, there is no entries for these states either."
+  ),
 
   sidebarLayout(
     sidebarPanel(
       selectInput(
         inputId = "var",
         label = "Pollutant",
-        choices = list(CO = "total_co", SO2 = "total_so2",
-                       O3 = "total_o3", NO2 = "total_no2",
-                       Total = "total_emm")
+        choices = list(
+          CO = "total_co", SO2 = "total_so2",
+          O3 = "total_o3", NO2 = "total_no2",
+          Total = "total_emm"
+        )
       ),
       radioButtons(
         inputId = "color",
@@ -113,16 +125,19 @@ page_two <- tabPanel(
   "Emissions over time",
   titlePanel("A Linechart of Emissions Over Time in the U.S."),
 
-  p("This chart shows the", strong("average yearly emissions levels"), "of each
+  p(
+    "This chart shows the", strong("average yearly emissions levels"), "of each
     of the four air pollutants in the dataset:", em("Carbon Monoxide (CO),"),
     em("Sulfur Dioxide (SO2),"), em("Ozone (O3),"), "and", em("Nitrogen Dioxide
     (NO2)."), "The data shown in this chart summarizes the emission levels of
     these harmful gases from the years", strong("2000-2016"), "and is intended
     to show the gases' emission levels", em("relative to each other."), "More
     importantly, this chart shows the changes in the average emission levels of
-    each gas through the years."),
+    each gas through the years."
+  ),
 
-  p("As can be seen in the chart, the average", em("CO"), "emissions have
+  p(
+    "As can be seen in the chart, the average", em("CO"), "emissions have
     consistently been the highest by a couple hundred parts per billion
     throughout the years. The average", em("O3, NO2, and SO2"),
     "emission levels have been quite close to each other all throughout and saw
@@ -131,7 +146,8 @@ page_two <- tabPanel(
     the year 2000, which is great news for the environment. However, a slight
     rise can also be observed towards the end of the", em("CO"), "emission
     graph, which could be an indicator of a shift into an upward trend in the
-    following years."),
+    following years."
+  ),
 
   sidebarLayout(
     sidebarPanel(
@@ -152,12 +168,14 @@ page_two <- tabPanel(
 
 page_three <- tabPanel(
   "Distribution of Emissions",
-  titlePanel("A distribution of the different types of Pollutants"),
+  titlePanel("A Distribution of the Different Types of Pollutants"),
 
-  p("This chart is a pie chart intended to show the relationship between the
+  p(
+    "This chart is a pie chart intended to show the relationship between the
     averages of", strong("four major pollutants:"), "Carbon Monoxide",
     strong("(CO)"), "Sulfur Dioxide", strong("(SO2)"), "Ozone", strong("(O3)"),
-    "and Nitrogen Dioxide", strong("(NO2)"), "in parts per billion."),
+    "and Nitrogen Dioxide", strong("(NO2)"), "in parts per billion."
+  ),
 
   p("When observing the chart below, three observations come into view. The
     first is that", strong("Carbon Monoxide has the largest mean."), "By
@@ -173,8 +191,10 @@ page_three <- tabPanel(
       checkboxGroupInput(
         inputId = "pollutants",
         label = "Pollutants",
-        choices = list(CO = "CO.Mean", SO2 = "SO2.Mean",
-                       NO2 = "NO2.Mean", O3 = "O3.Mean"),
+        choices = list(
+          CO = "CO.Mean", SO2 = "SO2.Mean",
+          NO2 = "NO2.Mean", O3 = "O3.Mean"
+        ),
         selected = c("CO.Mean", "SO2.Mean", "NO2.Mean", "O3.Mean"),
       )
     ),
@@ -187,14 +207,16 @@ page_three <- tabPanel(
 
 takeaways <- tabPanel(
   "Takeaways we have",
-  titlePanel("What we learned from this data", windowTitle = "Takeaways"),
+  titlePanel("What We Learned from this Data", windowTitle = "Takeaways"),
 
   p("After analyzing the original dataset and creating separate graphs to
     display the data in different ways, we were able to answer the three
     questions we had in the beginning:"),
-  p(em("1. What states are contributing the most to U.S. emissions?")),
-  p(em("2. How has the progression of emissions been from 2000 to 2016?")),
-  p(em("3. What pollutants are the most prevalent?")),
+  p(
+    em("1. What states are contributing the most to U.S. emissions?"), br(),
+    em("2. How has the progression of emissions been from 2000 to 2016?"),
+    br(), em("3. What pollutants are the most prevalent?")
+  ),
 
   p("Individually, each question pertains to one of the aforementioned graphs
     and their interactive abilities. Our data visualizations helped us to
@@ -219,17 +241,16 @@ takeaways <- tabPanel(
     California. Using this knowledge, we can move forward with more focused
     solutions and push for progress in these areas for the betterment of our
     environment and our future.")
-
 )
 
 my_ui <- fluidPage(
   includeCSS("style.css"),
   navbarPage(
-  "Major U.S. Pollutants",
-  overview,
-  page_one,
-  page_two,
-  page_three,
-  takeaways
+    "Major U.S. Pollutants",
+    overview,
+    page_one,
+    page_two,
+    page_three,
+    takeaways
   )
 )
